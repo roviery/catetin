b5135f049857d3cd2218f20565aead6148dbab8a
Nathanael Roviery <roviery@gmail.com>
2022-05-02 18:44:41 +0700

    Initialize Module Todo

--- todo/src/main/java/com/roviery/todo/ProfileFragment.kt
+++ todo/src/main/java/com/roviery/todo/ProfileFragment.kt
@@ -1,10 +1,11 @@
-package com.roviery.catetin
+package com.roviery.todo
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.roviery.catetin.R
 
 class ProfileFragment : Fragment() {
     override fun onCreateView(
