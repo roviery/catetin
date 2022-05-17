e06b63e367030e8ac7ca49f08c4012cff7760176
Nathanael Roviery <roviery@gmail.com>
2022-05-17 18:44:32 +0700

    Profile Modularization

---
+++ profile/src/main/java/com/roviery/profile/ProfileFragment.kt
@@ -0,0 +1,24 @@
+package com.roviery.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.roviery.catetin.R
+import com.roviery.profile.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+}
